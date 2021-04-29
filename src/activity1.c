0c600816b3c4549921cf618a84726725784f8d86
261833 <81299938+261833@users.noreply.github.com>
2021-04-29 20:29:47 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,1 +1,18 @@
+#include <activity1.h>
+#include <avr/io.h>
 
+int main(void)
+{
+
+    DDRB |= (1<<PB0);
+    DDRC &= ~(1<<PC0);
+    PORTC |= (1<<PC0);
+    while(1)
+    {
+     if ((PINC & (1<<PC0)) == 1)
+        PORTB |= (1<<PB0);
+     else
+        PORTB &= ~(1<<PB0);
+    }
+    return 0;
+}
