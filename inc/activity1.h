a1862c9e6b7bb587fa4289c6b47e09b106029f5b
261833 <81299938+261833@users.noreply.github.com>
2021-04-30 16:17:28 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,1 +1,11 @@
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY1_H_
+
+/*Include files*/
+#include <avr/io.h>     // HEADER FILE FOR ATMEGA328
+
+void led_init(void);   // IMPLEMENTATION OF ACTIVITY 1
+void init_pins(void);   // INITIALIZATION OF GPIO PINS
+
+#endif /** __ACTIVITY1_H_ */
 
